aa2a237a2dcacdf04bb6bc2a2b4c2b7caf8084a0
Alphazerfall <40797290+Alphazerfall@users.noreply.github.com>
2026-05-01 16:06:20 +0200

    refactor karaoke

--- karaoke_game/karaoke.py
+++ karaoke_game/karaoke.py
@@ -1,21 +1,41 @@
+import sys
 import pyglet
+from pyglet import window
 from audio import AudioInput
 
-audio = AudioInput()
-window = pyglet.window.Window(400, 200, caption="Karaoke")
 
-label = pyglet.text.Label("-- Hz", font_size=48, x=200, y=100, anchor_x="center", anchor_y="center")
+WINDOW_W, WINDOW_H = 1200, 600
 
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
 
-def update(dt):
-    pitch = audio.get_pitch()
-    label.text = f"{pitch:.0f} Hz"
+class Game:
+    def __init__(self):
+        self.audio = AudioInput()
+        self.label = pyglet.text.Label("-- Hz", font_size=48, x=WINDOW_W//2, y=WINDOW_H//2, anchor_x="center", anchor_y="center")
+    
+    def update(self, dt):
+        pitch = self.audio.get_pitch()
+        self.label.text = f"{pitch:.0f} Hz"
+        pass
 
-pyglet.clock.schedule_interval(update, 1/30)
+    def draw(self):
+        self.label.draw()
 
-with audio:
-    pyglet.app.run()
+
+if __name__ == "__main__":
+    win = pyglet.window.Window(WINDOW_W, WINDOW_H, caption="Karaoke")
+    game = Game()
+
+    @win.event
+    def on_key_press(symbol, modifiers):
+        if symbol == pyglet.window.key.Q:
+            pyglet.app.exit()
+            sys.exit(0)
+    
+    @win.event
+    def on_draw():
+        win.clear()
+        game.draw()
+
+    pyglet.clock.schedule_interval(game.update, 1/60)  # update at 60 FPS
+    with game.audio:
+        pyglet.app.run()
